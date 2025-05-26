c455ee4a9f2d001607ce261c3a75340f1ccb9cbb
helloMinh <phandinhminh48@gmail.com>
2025-05-26 15:21:28 +0700

    fixed auth

--- gatewayservice/src/main/java/com/se101/microservice/gatewayservice/config/SecurityConfig.java
+++ gatewayservice/src/main/java/com/se101/microservice/gatewayservice/config/SecurityConfig.java
@@ -2,10 +2,11 @@ package com.se101.microservice.gatewayservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.HttpStatusServerEntryPoint;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -19,9 +20,11 @@ public class SecurityConfig {
                 .pathMatchers("/actuator/health").permitAll()
                 .anyExchange().authenticated()
             )
-            .oauth2Login(withDefaults())
             .oauth2ResourceServer(oauth2 -> oauth2
-                .jwt(withDefaults())
+                .jwt(jwt -> {})
+            )
+            .exceptionHandling(exceptionHandling -> exceptionHandling
+                .authenticationEntryPoint(new HttpStatusServerEntryPoint(HttpStatus.UNAUTHORIZED))
             )
             .csrf(csrf -> csrf.disable())
             .build();
